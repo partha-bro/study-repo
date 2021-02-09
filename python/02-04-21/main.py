9244d6544788490da8d7be240fce20cb6e49e872
Partha-bro <sarathi.partha95@gmail.com>
2021-02-09 08:44:11 +0530

    Added into about Tuples in python
    [DATE: 09-02-21] :: part 1
    python/02-04-21/main.py --
        Tuples

--- python/02-04-21/main.py
+++ python/02-04-21/main.py
@@ -1,7 +1,7 @@
 ### Date: 04-02-21 ###
 
 """
-    1.             Line = 20
+    1. Tuples             Line = 20
     2. 
     3. 
     4. 
@@ -14,5 +14,19 @@
     11. 
 """
 
-
+## Truple
+"""
+    1. tuple data structure
+    2. tuple can store any data type
+    3. most important tuples are immutable, 
+        once tuple is created you cannot update [ no append, no insert, no pop, no remove] 
+    4. data inside tuple 
+    syntax: list_name = ['one,'two','three']
+            tuple_name = ('one,'two','three')
+    methods:-
+    count,index,len function,slicing
+"""
+days = ('mon','tue','wed','thu','fri','sat','sun')
+print(f'{days}')
+days[1] = 1     # tuple can not suppot this
 
