0ee3efb5b605cf56da60bc0581bcf76ef0c0a510
Partha-bro <sarathi.partha95@gmail.com>
2021-02-10 21:36:28 +0530

    create php file in 02-10-21

---
+++ php/02-10-21/index.php
@@ -0,0 +1,13 @@
+<?php
+### TOPIC NAME: Handling Exceptions ###
+
+/*
+    1. Raise and Catch an Exception                             Line = 
+    2. Throw an Custom Exception                                Line = 
+    3. Catch Multiple Exceptions                                Line = 
+    4. Finally Block                                            Line = 
+    5. Raise and Catch Errors                                   Line = 
+*/
+
+## Raise and Catch an Exception
+    
