7e8fcfc6013255be6432acbec065cc317d18aff5
Partha-bro <sarathi.partha95@gmail.com>
2021-03-02 20:52:10 +0530

    Added about oops in php

---
+++ php/03-03-21/index.php
@@ -0,0 +1,31 @@
+<?php
+### TOPIC NAME: PHP OOP Basics ###
+
+/*
+    1. What is OOP                                                      Line = 
+    2. What is Class                                                    Line = 
+    3. How to Define a Class                                            Line = 
+    4. What is Property and Methods                                     Line = 
+    5. What is Objects                                                  Line = 
+    6. How to Define Objects                                            Line = 
+    7. Define Methods with Parameters and Return Value                  Line = 
+    8. Use case of Users with Class and Objects                         Line = 
+    9. Project Calculate Employee Salary                                Line = 
+    10. Exercise 1 Create a Bank Class, Object, Property and Methods    Line = 
+    11. Exercise 2 Create a Files Class, Object, Property and Methods   Line = 
+    12. Assignment                                                      Line = 
+*/
+
+// 1. What is OOP        
+                                                  
+// 2. What is Class                                                    
+// 3. How to Define a Class                                           
+// 4. What is Property and Methods                                    
+// 5. What is Objects                                                  
+// 6. How to Define Objects                                           
+// 7. Define Methods with Parameters and Return Value                  
+// 8. Use case of Users with Class and Objects                         
+// 9. Project Calculate Employee Salary                               
+// 10. Exercise 1 Create a Bank Class, Object, Property and Methods    
+// 11. Exercise 2 Create a Files Class, Object, Property and Methods   
+// 12. Assignment                                                     
