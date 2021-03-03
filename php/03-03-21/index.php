31e802af17c832a8983cb948b1865af0e278a7ac
Partha-bro <sarathi.partha95@gmail.com>
2021-03-03 21:16:19 +0530

    Added about 2 exercise of bank and employee salary program

--- php/03-03-21/index.php
+++ php/03-03-21/index.php
@@ -2,18 +2,18 @@
 ### TOPIC NAME: PHP OOP Basics ###
 
 /*
-    1. What is OOP                                                      Line = 
-    2. What is Class                                                    Line = 
-    3. How to Define a Class                                            Line = 
-    4. What is Property and Methods                                     Line = 
-    5. What is Objects                                                  Line = 
-    6. How to Define Objects                                            Line = 
-    7. Define Methods with Parameters and Return Value                  Line = 
-    8. Use case of Users with Class and Objects                         Line = 
-    9. Project Calculate Employee Salary                                Line = 
-    10. Exercise 1 Create a Bank Class, Object, Property and Methods    Line = 
-    11. Exercise 2 Create a Files Class, Object, Property and Methods   Line = 
-    12. Assignment                                                      Line = 
+    1. What is OOP                                                      Line = 19
+    2. What is Class                                                    Line = 39
+    3. How to Define a Class                                            Line = 48
+    4. What is Property and Methods                                     Line = 68
+    5. What is Objects                                                  Line = 92
+    6. How to Define Objects                                            Line = 102
+    7. Define Methods with Parameters and Return Value                  Line = 112
+    8. Use case of Users with Class and Objects                         Line = 137
+    9. Project Calculate Employee Salary                                Line = 138
+    10. Exercise 1 Create a Bank Class, Object, Property and Methods    Line = 165
+    11. Exercise 2 Create a Files Class, Object, Property and Methods   Line = 220
+    12. THE END                                                     
 */
 
 // 1. What is OOP        
@@ -134,8 +134,117 @@
     $file->readFile('../03-01-21/testfolder/config.ini');
     echo "<hr/>"; 
 
-// 8. Use case of Users with Class and Objects                         
-// 9. Project Calculate Employee Salary                               
-// 10. Exercise 1 Create a Bank Class, Object, Property and Methods    
-// 11. Exercise 2 Create a Files Class, Object, Property and Methods   
-// 12. Assignment                                                     
+// 8. Use case of Users with Class and Objects
+// 9. Project Calculate Employee Salary
+    class Employee{
+        public $id;
+        public $name;
+        public $workingHourPerDay = 8.5;
+        public $hourlyRate = 59;
+        public $totalLeaveTaken;
+        public $workingDays;
+
+        public function getSalaryAmount($totalDays) : int
+        {
+            # code...
+            $this->workingDays = $totalDays - $this->totalLeaveTaken;
+            $salary = $this->workingDays * $this->workingHourPerDay * $this->hourlyRate;
+            return $salary;
+        }
+    }
+
+    $emp_1 = new Employee;
+    $emp_1->id = 'DS143';
+    $emp_1->name = 'Arjun';
+    $emp_1->totalLeaveTaken = 0;
+    $salary = $emp_1->getSalaryAmount(23);
+    echo "$emp_1->name of Salary is $salary";
+
+    echo "<hr/>";
+
+// 10. Exercise 1 Create a Bank Class, Object, Property and Methods
+    /*
+        1. Create a bank class 
+        2. Define properties of bank class
+            - accno
+            - name
+            - balance
+        3. Define Methods
+            - depositAmount($amt)
+            - deductAmount($amt)
+            - checkBalance()
+        4. Create an account for user
+        5. Checkbalance, Depositeamount, Deductamout
+    */
+        class Bank{
+            public $accno;
+            public $name;
+            public $balance;
+        
+            public function depositAmount($amt){
+                #code...
+                $this->balance += $amt;
+                echo "+$amt Credit successfully: ".$this->balance."<br/>";
+            }
+
+            public function deductAmount($amt){
+                #code...
+                $this->balance -= $amt;
+                echo "-$amt Debit successfully: ".$this->balance."<br/>";
+            }
+
+            public function checkBalance(){
+                echo "Dear $this->name, Your Main Balance is: ".$this->balance." For reforence, Your account no: $this->accno <br/>";
+            }
+        }
+
+        $accHolder_1 = new Bank;
+        $accHolder_1->accno = 506;
+        $accHolder_1->name = 'Mr. Hacker';
+        $accHolder_1->balance = 300000;
+
+        // Check balance
+        $accHolder_1->checkBalance();
+
+        // Deposite amount
+        $accHolder_1->depositAmount(500);
+
+        // Deposite amount
+        $accHolder_1->deductAmount(200);
+
+        // Check balance
+        $accHolder_1->checkBalance();
+
+        echo "<hr/>";
+
+// 11. Exercise 2 Create a Files Class, Object, Property and Methods
+    /*
+        1. Create a file class
+        2. Create methods
+            - displayContent with filename as input parameter
+            - getContent with filename as input parameter 
+        3. Pass the Test.txt and displayContent and getContent.
+    */
+    class File
+    {
+        public $content;
+
+        public function getContent($filename){
+            // $this->content = file_get_contents($filename);
+
+            $fileHandler = fopen($filename,'r');
+            $fsize = filesize($filename);
+            $this->content = fread( $fileHandler, $fsize);
+            fclose($fileHandler);
+        }
+
+        public function displayContent()
+        {
+            # code...
+            echo "Content from File is : ".$this->content;
+        }
+    }
+    
+    $file_1 = new File;
+    $file_1->getContent('../03-01-21/testfolder/config.ini');
+    $file_1->displayContent();
