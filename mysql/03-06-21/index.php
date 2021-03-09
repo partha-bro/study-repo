060e1a73c1e1c8a84f08b37ca7b64317e60ea3f1
arjun <sarathi.partha95@gmail.com>
2021-03-09 18:05:12 +0530

    About Regular expression

--- mysql/03-06-21/index.php
+++ mysql/03-06-21/index.php
@@ -69,17 +69,33 @@ echo "<hr/>";
 
 			preg_match(pattern, string)
 			preg_match_all(pattern, string, array)
+
+		For Online tool to use testing our Regular expression: https://regex101.com/
+		For learn : https://www.youtube.com/watch?v=3RlDZEa6gW8
+
+		Notation for use in regular expression
+		\w : matches any word character (equivalent to [a-zA-Z0-9_])
+		\W : matches any non-word character (equivalent to [^a-zA-Z0-9_])
+		\d : matches a digit (equivalent to [0-9])
+		\D : matches any character that's not a digit (equivalent to [^0-9])
+		\s : matches any whitespace character (equivalent to [\r\n\t\f\v ])
+		\S : matches any non-whitespace character (equivalent to [^\r\n\t\f\v ])
+		\b : assert position at a word boundary: (^\w|\w$|\W\w|\w\W) eg. \bword\b
+		.  : matches any character (except for line terminators) eg. fi.. = file | f. = fi
+		\. : matches the character . literally (case sensitive) [use any special char \[.?$#@!%&*.etc]]
+		\v : matches any vertical whitespace character
+
 	*/
 	$string = "PHP is the web scripting php language of choice.";
 
-	$exp = preg_match("/PHP/", $string); 						// case sensitive
-	$exp_1 = preg_match("/php/i", $string); 					// non case sensitive
-	$exp_2 = preg_match_all("/php/i", $string, $arr); 			// non case sensitive with all data
-	$exp_3 = preg_match_all("/php|web|the/i", $string, $arr); 	// more than one data 
-	$exp_3 = preg_match_all("/p|w|t/i", $string, $arr); 		// more than one char
-	$exp_3 = preg_match_all("/[pwt]/i", $string, $arr); 		// more than one char without using |
-	$exp_3 = preg_match_all("/[^pwt]/i", $string, $arr); 		// print all char except p,w,t char
-	$exp_3 = preg_match_all("/[a-d]/i", $string, $arr); 		// print a to d chars 
+	$exp = preg_match("/PHP/", $string); 							// case sensitive
+	$exp_1 = preg_match("/php/i", $string); 						// non case sensitive
+	$exp_2 = preg_match_all("/php/i", $string, $arr); 				// non case sensitive with all data
+	$exp_3 = preg_match_all("/php|web|the/i", $string, $arr); 		// more than one data 
+	$exp_3 = preg_match_all("/p|w|t/i", $string, $arr); 			// more than one char
+	$exp_3 = preg_match_all("/[pwt]/i", $string, $arr); 			// more than one char without using |
+	$exp_3 = preg_match_all("/[^pwt]/i", $string, $arr); 			// print all char except p,w,t char
+	$exp_3 = preg_match_all("/[a-d]/i", $string, $arr); 			// print a to d chars 
 	$exp_3 = preg_match_all("/[a-zA-Z0-9]/i", $string, $arr); 		// print a to z chars and A to Z and 0 to 9 
 
 	if ($exp_3) {
@@ -89,5 +105,58 @@ echo "<hr/>";
 		echo "A match was not found.";
 	}
 
+	echo "<pre>";
+	print_r($arr);
+
+echo "<hr/>";
+
+## Example 1
+	$string = "file1 file2 file3 file# file? file5 file6";
+	$exp_no = preg_match_all('/file[1-9]/', $string, $arr);
+	echo "<pre>";
+	print_r($arr);
+
+	$exp_no = preg_match_all('/file[^1-9]/', $string, $arr);
+	echo "<pre>";
+	print_r($arr);
+
+echo "<hr/>";
+	/*
+		Regular Expressions: Quantifiers
+
+		* 			: 0 or more
+		+ 			: 1 or more
+		? 			: 0 or one
+		{3} 		: Exact Number
+		{3,5} 		: Range of Numbers(minimum, maximum)
+		{3,} 		: Minimum Number to infinite
+	*/
+	$string = "word 1word 12word 123word 1234word pphhhhhpppppppp january febuary jan feb";
+	$exp_no = preg_match_all('/\d*word/', $string, $arr);
+	echo "<pre>";
+	print_r($arr);
+
+	$exp_no = preg_match_all('/\d+word/', $string, $arr);
+	echo "<pre>";
+	print_r($arr);
+
+	$exp_no = preg_match_all('/p{2}h{1}/', $string, $arr);
+	echo "<pre>";
+	print_r($arr);
+
+	$exp_no = preg_match_all('/p{2}h{1,3}/', $string, $arr);
+	echo "<pre>";
+	print_r($arr);
+
+	$exp_no = preg_match_all('/p{2}h{1,}p{1,4}/', $string, $arr);
+	echo "<pre>";
+	print_r($arr);
+
+	$exp_no = preg_match_all('/12?word/', $string, $arr);
+	echo "<pre>";
+	print_r($arr);
+
+	// find jan or january month
+	$exp_no = preg_match_all('/jan(uary)?/', $string, $arr);
 	echo "<pre>";
 	print_r($arr);
