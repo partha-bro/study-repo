6863fa56fd6dd9a645c126ba2116b6a173d294b2
arjun <sarathi.partha95@gmail.com>
2021-09-06 18:23:58 +0530

    new jquery study

---
+++ jQuery/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Index || jQuery</title>
+</head>
+<body>
+	<input type="button" name="btn" id="btn" value="click">
+<div id="box" style="background-color:red;height: 100px;width: 100px;margin-top: 500px;">
+	
+</div>
+
+
+
+<script type="text/javascript" src="js/jquery.js"></script>
+<script type="text/javascript">
+	$(document).ready(function(){
+		$('#btn').click(function(){
+			$('#box').slideUp(5000);
+		});
+	});
+	
+</script>
+</body>
+</html>
